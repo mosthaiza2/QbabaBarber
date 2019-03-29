e27523775e3626f4b5b5c2ca973a6c913f1970ad
Krisana Tubsaithong <mosthsiza@gmail.com>
2019-03-29 17:15:41 +0700

    Update 2

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,6 +9,7 @@ import { AboutPage } from '../pages/about/about';
 import { ContactPage } from '../pages/contact/contact';
 import { HomePage } from '../pages/home/home';
 import { TabsPage } from '../pages/tabs/tabs';
+import { DetailPage } from '../pages/detail/detail';
 
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
@@ -19,7 +20,8 @@ import { SplashScreen } from '@ionic-native/splash-screen';
     AboutPage,
     ContactPage,
     HomePage,
-    TabsPage
+    TabsPage,
+    DetailPage
   ],
   imports: [
     BrowserModule,
@@ -32,7 +34,8 @@ import { SplashScreen } from '@ionic-native/splash-screen';
     AboutPage,
     ContactPage,
     HomePage,
-    TabsPage
+    TabsPage,
+    DetailPage
   ],
   providers: [
     StatusBar,
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { NavController, NavParams } from 'ionic-angular';
+import { DetailPage } from '../detail/detail';
 import {Http} from '@angular/http';
 import 'rxjs/add/operator/map';
 
@@ -11,8 +12,11 @@ export class HomePage {
   barbershop:any=0;
   constructor(public navCtrl: NavController,public navParam: NavParams, public http: Http) {
     this.http.get('http://localhost:8080/barbershop')
-    .map(res => res.json()).subscribe(data => {this.barbershop= data;});
-    
+    .map(res => res.json()).subscribe(data => {this.barbershop= data});
+  }
+  showDetail(id)
+  {
+    this.navCtrl.push(DetailPage,{barberid:id});
   }
 
 }
