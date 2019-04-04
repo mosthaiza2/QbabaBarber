6b712438d30c8329acb54937c0b2388b12630f93
Krisana Tubsaithong <mosthsiza@gmail.com>
2019-04-04 14:23:15 +0700

    update 04/04/2019

--- src/pages/result/result.ts
+++ src/pages/result/result.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { DetailPage } from '../detail/detail';
 import 'rxjs/add/operator/map';
 import {Http} from '@angular/http';
 
@@ -22,6 +23,10 @@ export class ResultPage {
     let url = "http://localhost:8080/barbershop/name/" + bid;
     console.log(url)
     this.http.get(url).map(res => res.json()).subscribe(data => {this.barbershop = data});
+  }
+  showDetail(id)
+  {
+    this.navCtrl.push(DetailPage,{barberid:id});
   }
   ionViewDidLoad() {
     console.log('ionViewDidLoad ResultPage');
